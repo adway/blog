de017ad4cce3fd61b826437e2c789b888f202000
Adway Wadekar <adway@adway.io>
2020-06-14 21:19:52 -0400

    Books!

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,6 +5,7 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-posts-2020-05-25-a-defense-of-the-2020-ap-exams-md": () => import("./../src/pages/posts/2020-05-25-a-defense-of-the-2020-ap-exams.md" /* webpackChunkName: "component---src-pages-posts-2020-05-25-a-defense-of-the-2020-ap-exams-md" */),
-  "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md": () => import("./../src/pages/posts/2020-06-01-A_Reflection_on_the_Events_Surrounding_the_Death_of_George_Floyd.md" /* webpackChunkName: "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md" */)
+  "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md": () => import("./../src/pages/posts/2020-06-01-A_Reflection_on_the_Events_Surrounding_the_Death_of_George_Floyd.md" /* webpackChunkName: "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md" */),
+  "component---src-pages-posts-2020-06-14-summer-reading-mdx": () => import("./../src/pages/posts/2020-06-14-Summer_Reading.mdx" /* webpackChunkName: "component---src-pages-posts-2020-06-14-summer-reading-mdx" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/src/pages/index.js"))),
   "component---src-pages-posts-2020-05-25-a-defense-of-the-2020-ap-exams-md": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/src/pages/posts/2020-05-25-a-defense-of-the-2020-ap-exams.md"))),
-  "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/src/pages/posts/2020-06-01-A_Reflection_on_the_Events_Surrounding_the_Death_of_George_Floyd.md")))
+  "component---src-pages-posts-2020-06-01-a-reflection-on-the-events-surrounding-the-death-of-george-floyd-md": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/src/pages/posts/2020-06-01-A_Reflection_on_the_Events_Surrounding_the_Death_of_George_Floyd.md"))),
+  "component---src-pages-posts-2020-06-14-summer-reading-mdx": hot(preferDefault(require("/Volumes/Samsung_T5/src/blog/src/pages/posts/2020-06-14-Summer_Reading.mdx")))
 }
 
